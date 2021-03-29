f0d67d210086c6c3debc43604b8fccb5aa9484e4
jchen <jchen98@163.com>
2021-03-29 16:51:53 +0800

    生成验证码

---
+++ src/main/java/com/jchen/community/config/KaptchaConfig.java
@@ -0,0 +1,36 @@
+package com.jchen.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * Kaptcha配置类
+ *
+ * @Auther: jchen
+ * @Date: 2021/03/29/16:29
+ */
+@Configuration
+public class KaptchaConfig {
+
+    @Bean
+    public Producer kaptchaProducer(){
+        Properties properties = new Properties();
+        properties.setProperty("kaptcha.image.width","100");
+        properties.setProperty("kaptcha.image.height","40");
+        properties.setProperty("kaptcha.textproducer.font.size","32");
+        properties.setProperty("kaptcha.textproducer.font.color","0,0,0");
+        properties.setProperty("kaptcha.textproducer.char.string","0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        properties.setProperty("kaptcha.textproducer.char.length","4");
+        properties.setProperty("kaptcha.noise.impl","com.google.code.kaptcha.impl.NoNoise");
+        DefaultKaptcha kaptcha=new DefaultKaptcha();
+        Config config=new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
