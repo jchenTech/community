12028f73cb0cd35047e647003f1dcc714ec1c559
jchen <jchen98@163.com>
2021-03-29 12:47:11 +0800

    实现发送邮件功能

---
+++ src/test/java/com/jchen/community/MailTests.java
@@ -0,0 +1,39 @@
+package com.jchen.community;
+
+import com.jchen.community.util.MailClient;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+
+/**
+ * @Auther: jchen
+ * @Date: 2021/03/29/12:10
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("jchen98@163.com", "TEST", "Welcome");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("jchen98@163.com", "HTML", content);
+    }
+}
