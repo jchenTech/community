93410d654391340e1983aa35519bfd7fafaa8fa9
jchen <jchen98@163.com>
2021-03-30 21:19:50 +0800

    实现私信列表功能

---
+++ src/main/java/com/jchen/community/service/MessageService.java
@@ -0,0 +1,40 @@
+package com.jchen.community.service;
+
+import com.jchen.community.dao.MessageMapper;
+import com.jchen.community.entity.Message;
+import com.jchen.community.util.SensitiveFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @Auther: jchen
+ * @Date: 2021/03/30/20:07
+ */
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageMapper messageMapper;
+
+    public List<Message> findConversations(int userId, int offset, int limit) {
+        return messageMapper.selectConversations(userId, offset, limit);
+    }
+
+    public int findConversationCount(int userId) {
+        return messageMapper.selectConversationCount(userId);
+    }
+
+    public List<Message> findLetters(String conversationId, int offset, int limit) {
+        return messageMapper.selectLetters(conversationId, offset, limit);
+    }
+
+    public int findLetterCount(String conversationId) {
+        return messageMapper.selectLetterCount(conversationId);
+    }
+
+    public int findLetterUnreadCount(int userId, String conversationId) {
+        return messageMapper.selectLetterUnreadCount(userId, conversationId);
+    }
+}
