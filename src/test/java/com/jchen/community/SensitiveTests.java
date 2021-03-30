b32c8c24a3da53132afd0e4c8bab9419cb9b04ca
jchen <jchen98@163.com>
2021-03-30 11:47:09 +0800

    利用前缀树进行敏感词过滤

---
+++ src/test/java/com/jchen/community/SensitiveTests.java
@@ -0,0 +1,30 @@
+package com.jchen.community;
+
+import com.jchen.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+/**
+ * @Auther: jchen
+ * @Date: 2021/03/30/11:30
+ */
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博，可以嫖娼，可以吸毒，可以开票";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以☆赌☆博☆,可以☆嫖☆娼☆,可以☆吸☆毒☆,可以☆开☆票☆,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
